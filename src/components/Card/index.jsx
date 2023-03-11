3c11dfc29cb274841f5a3e21197b0d27e9c52deb
SayanDeveloper <samantasayan23@gmail.com>
2023-03-11 23:19:53 +0530

    ✨ dashboard api integration done

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Card = () => {
+const Card = ({movie}) => {
+    const navigate = useNavigate()
+
     return (
-        <div className='w-[calc(50vw-35px)] xs:w-[calc(50vw-55px)] fl-xs:w-[200px] '>
-            <img src="https://image.tmdb.org/t/p/original/rVdd23QuT5rHX7lZvuAkRRUkeCZ.jpg" className='h-[auto] fl-xs:h-[300px] object-cover rounded-[20px] mb-[8px]' />
-            <p className='text-center'>Three Colors Collection lorem </p>
+        <div className='w-[calc(50vw-35px)] xs:w-[calc(50vw-55px)] fl-xs:w-[200px] cursor-pointer' onClick={() => navigate(`/movie/${movie.id}`)}>
+            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} className='h-[auto] fl-xs:h-[300px] object-cover rounded-[20px] mb-[8px]' />
+            <p className='text-center'>{movie.title} </p>
             <div className='flex flex-row justify-center items-center gap-[10px] text-[#6f7075] text-sm mt-[2px]'>
-                <p>Movie</p>
+                <p className='capitalize'>{movie.media_type}</p>
                 <div className='rounded-[50%] bg-[#6f7075] h-[5px] w-[5px]'></div>
-                <p>2020</p>
+                <p>{movie.release_date.slice(0, 4)}</p>
             </div>
         </div>
     )
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import Card from '../../components/Card'
 
 const Dashboard = () => {
-  return (
-    <>
-        <h1 className="text-3xl font-bold text-center mb-8 pt-5">
-            Trending Movies
-        </h1>
-        <div className='flex flex-row gap-[30px] flex-wrap mx-auto fl-xs:max-w-[430px] fl-sm:max-w-[661px] fl-md:max-w-[895px] fl-lg:max-w-[1120px]'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-        </div>
-    </>
-  )
+    const [trendingMovies, setTrendingMovies] = useState([]);
+
+    const trendingMovieFetcher = async () => {
+        const res = await axios.get(process.env.REACT_APP_BASE_URL+"/trending/movie/week?api_key="+process.env.REACT_APP_TMDB_KEY)
+        console.log(process.env.REACT_APP_BASE_URL+"/trending/movie/week?api_key="+process.env.REACT_APP_TMDB_KEY)
+        console.log(res.data.results)
+        setTrendingMovies(res.data.results)
+    }
+
+    useEffect(() => {
+        trendingMovieFetcher()
+    }, [])
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold text-center mb-8 pt-5">
+                Trending Movies
+            </h1>
+            <div className='flex flex-row gap-[30px] flex-wrap mx-auto fl-xs:max-w-[430px] fl-sm:max-w-[661px] fl-md:max-w-[895px] fl-lg:max-w-[1120px]'>
+                {trendingMovies.map((movie, index) => (
+                    <Card movie={movie} />
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default Dashboard
